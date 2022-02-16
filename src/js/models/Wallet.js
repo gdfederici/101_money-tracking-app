f7e3f9f2ba28a171f95af598b7fd3798beea5d30
gdfederici <gdfederici@gmail.com>
2022-02-16 11:13:10 +0100

    Create addOperation flow. Add saveWallet to store the wallet in te storage.

--- src/js/models/Wallet.js
+++ src/js/models/Wallet.js
@@ -1,3 +1,7 @@
+var OpType = {
+    OUT: 0,
+    IN: 1
+}
 function getWallet() {
     var wallet = localStorage.getItem('wallet');
     if(!wallet) {
@@ -8,6 +12,9 @@ function getWallet() {
     }
     return JSON.parse(wallet);
 }
+function saveWallet(wallet) {
+    localStorage.setItem('wallet', JSON.stringify(wallet));
+}
 function Wallet() {
     var balance = 0;
     var operations = [];
@@ -17,7 +24,19 @@ function Wallet() {
         operations = wallet.operations; 
     }
     this.addOperation = function() {
-
+        var operation = {
+            amount: op.amount,
+            description: op.description,
+            type: op.type,
+            date: new Date().getTime()
+        }
+        if(op.type === OpType.IN) {
+            balance += operation.amount;
+        } else if(op.type === OpType.OUT) {
+            balance -= operation.amount;
+        }
+        operations.push(operation);
+        safeWallet();
     }
     this.removeOperation = function() {
     
