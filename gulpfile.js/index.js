2d16992d226bf64a3769a2947f9f38183802da2c
gdfederici <gdfederici@gmail.com>
2022-02-18 15:10:51 +0100

    Add serve task.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,8 +2,9 @@ const gulp = require("gulp");
 const series = gulp.series;
 const viewTasks = require("./viewTasks");
 const jsTasks = require("./jsTasks");
+const serveTasks = require("./serveTasks");
 
-const dev = series(viewTasks.compileIndex, jsTasks.copyJs, jsTasks.watchJS, viewTasks.watchIndex /*, serve, watchHtml */);
+const dev = series(viewTasks.compileIndex, jsTasks.copyJs, jsTasks.watchJS, viewTasks.watchIndex, serveTasks.serve);
 
 module.exports = {
     dev: dev
